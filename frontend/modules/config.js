913090ed0a4778ca3f46d81081135506a324bdf2
SouthHorizons76 <1076morf@gmail.com>
2026-03-24 10:46:40 +0800

    add ICP beian number to site footer

--- frontend/modules/config.js
+++ frontend/modules/config.js
@@ -10,7 +10,7 @@ window.EFTForge.config = {
     IS_LOCAL_DEV: _isLocalDev,
 
     APP_VERSION:    "v1.0.0",
-    APP_BUILD_DATE: "2026-03-23T16:20:14.367Z", // UTC - run new Date().toISOString() in console when bumping version
+    APP_BUILD_DATE: "2026-03-24T02:45:58.744Z", // UTC - run new Date().toISOString() in console when bumping version
 
     CALIBER_DISPLAY_MAP: {
         "Caliber20x1mm":      "20x1mm disk",
