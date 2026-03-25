d4a2550c9f4b64c731c55db02fde397332697046
SouthHorizons76 <1076morf@gmail.com>
2026-03-25 15:15:25 +0800

    fix mobile community card scaling

--- frontend/modules/config.js
+++ frontend/modules/config.js
@@ -10,7 +10,7 @@ window.EFTForge.config = {
     IS_LOCAL_DEV: _isLocalDev,
 
     APP_VERSION:    "v1.0.1",
-    APP_BUILD_DATE: "2026-03-25T06:45:57.662Z", // UTC - run new Date().toISOString() in console when bumping version
+    APP_BUILD_DATE: "2026-03-25T07:14:54.548Z", // UTC - run new Date().toISOString() in console when bumping version
 
     CALIBER_DISPLAY_MAP: {
         "Caliber20x1mm":      "20x1mm disk",
