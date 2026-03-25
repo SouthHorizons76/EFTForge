34e7a9118df709c5b87855082dc03697665e4094
SouthHorizons76 <1076morf@gmail.com>
2026-03-25 14:46:24 +0800

    bump version to v1.0.1

--- frontend/modules/config.js
+++ frontend/modules/config.js
@@ -9,8 +9,8 @@ window.EFTForge.config = {
 
     IS_LOCAL_DEV: _isLocalDev,
 
-    APP_VERSION:    "Development Build",
-    APP_BUILD_DATE: "", // UTC - run new Date().toISOString() in console when bumping version
+    APP_VERSION:    "v1.0.1",
+    APP_BUILD_DATE: "2026-03-25T06:45:57.662Z", // UTC - run new Date().toISOString() in console when bumping version
 
     CALIBER_DISPLAY_MAP: {
         "Caliber20x1mm":      "20x1mm disk",
